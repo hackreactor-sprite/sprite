f4a4aa76a146ddc25d757b418e821bcc9db68507
Kathy Ye <kathyye15@gmail.com>
2023-01-26 12:04:37 -0800

    installed eslint

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const router = require('./router.js');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
 const PORT = process.env.port || 3000;
 
 app.listen(PORT);
 console.log(`Listening at http://localhost:${PORT}`);
-
 app.use('/', router);
