b5f237971445c4d9dd3b7d43a7f212b8c694d1eb
Kathy Ye <kathyye15@gmail.com>
2023-01-27 23:22:41 -0800

    Add relatedProductsEntry component

---
+++ client/src/layout/RelatedProductsEntry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RelatedProductsEntry({allProducts, setAllProducts, currProduct, setCurProduct}) {
+  return (
+    <div>
+      <h2>Related Products</h2>
+      <div>RelatedProductsList</div>
+    </div>
+  );
+}
