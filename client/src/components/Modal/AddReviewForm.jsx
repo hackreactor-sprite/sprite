f1e263514c43a77df9e2d855f7bf3916d3572bab
samuel ho <samuelho343@gmail.com>
2023-01-31 10:27:01 -0800

    initial commit component + css

---
+++ client/src/components/Modal/AddReviewForm.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+export default function AddQuestionForm({ curProduct, handleModal }) {
+  const [questionForm, setQuestionForm] = useState({
+    name: '',
+    body: '',
+    email: '',
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formObj = {
+      body: e.target.body.value,
+      name: e.target.name.value,
+      email: e.target.email.value,
+      product_id: curProduct.id,
+    };
+    console.log('Submitted Form :', formObj);
+    axios
+      .post('/qa/questions', formObj)
+      .then(() => {
+        console.log('success');
+        handleModal();
+      })
+      .catch((err) => new Error(err));
+  }
+  return (
+    <form className="modal-form" onSubmit={handleSubmit}>
+      <h3>Ask your Question</h3>
+      <small>
+        {'About the '}
+        {curProduct.name}
+      </small>
+      <div>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={questionForm.name}
+          onChange={(e) => {
+            const newInput = { ...questionForm };
+            newInput.name = e.target.value;
+            setQuestionForm(newInput);
+          }}
+        />
+        <small>
+          For privacy reasons, do not use your full name or email address
+        </small>
+      </div>
+      <textarea
+        type="text"
+        name="body"
+        placeholder="Your Question?"
+        value={questionForm.body}
+        onChange={(e) => {
+          const newInput = { ...questionForm };
+          newInput.body = e.target.value;
+          setQuestionForm(newInput);
+        }}
+        maxLength="1000"
+        rows="4"
+        cols="50"
+      />
+      <div>
+        <input
+          type="text"
+          name="email"
+          placeholder="Please input your email"
+          value={questionForm.email}
+          onChange={(e) => {
+            const newInput = { ...questionForm };
+            newInput.email = e.target.value;
+            setQuestionForm(newInput);
+          }}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+AddQuestionForm.propTypes = {
+  curProduct: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+  }).isRequired,
+  handleModal: PropTypes.func.isRequired,
+};
---
+++ client/src/components/RatingReview/ReviewItem.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from 'react';
+import Helpful from '../Reusable/Helpful';
+import Report from '../Reusable/Report';
+
+export default function ReviewItem({ review }) {
+  const [reported, setReported] = useState(false);
+  const [showBody, setShowBody] = useState(false);
+  const options = {
+    month: 'long',
+    year: 'numeric',
+    day: 'numeric',
+  };
+  const convertedDate = new Date(review.date).toLocaleDateString(
+    undefined,
+    options,
+  );
+
+  return (
+    <div className="review-item">
+      <div className="reviewitem-header">
+        <div>{review.rating}</div>
+        <small>
+          {'by '}
+          {review.reviewer_name}
+          {', '}
+          {convertedDate}
+        </small>
+      </div>
+      <h5>{review.summary.substring(0, 60)}</h5>
+      <p>{!showBody ? review.body : review.body.substring(0, 250)}</p>
+      <div className="review-item-photos">
+        {review.photos.map((photo) => (
+          // eslint-disable-next-line jsx-a11y/img-redundant-alt
+          <img src={photo} alt="review-photo" />
+        ))}
+      </div>
+      {review.recommend ? <div>I recommend this product</div> : null}
+      {review.response ? <div>{review.response}</div> : null}
+      {review.photos.map((photo) => (
+        // eslint-disable-next-line jsx-a11y/img-redundant-alt
+        <img src={photo} alt="review-photo" />
+      ))}
+      <div className="small-container review-detail">
+        <Helpful helpful={review.helpfulness} answerid={review.id} />
+        {!reported ? (
+          <Report
+            id={review.id}
+            type="review"
+            setReported={setReported}
+            reported={reported}
+          />
+        ) : (
+          <small>Reported</small>
+        )}
+      </div>
+    </div>
+  );
+}
