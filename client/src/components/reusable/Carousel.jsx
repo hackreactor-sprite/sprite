795cb2bb721bd52bb38028870cc5aaef1af199dd
Kathy Ye <kathyye15@gmail.com>
2023-01-31 00:58:22 -0800

    implemented related products carousel

--- client/src/components/reusable/Carousel.jsx
+++ client/src/components/reusable/Carousel.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const fakeData = ['product1', 'product2', 'product3', 'product4', 'product5'];
+// const fakeData = ['product1', 'product2', 'product3', 'product4', 'product5'];
 
-function Product({ name }) {
-  return (
-    <div style={{
-      minWidth: '100px', width: '100px', height: '150px', backgroundColor: 'cyan',
-    }}
-    >
-      {name}
-    </div>
-  );
-}
+// function Product({ name }) {
+//   return (
+//     <div style={{
+//       minWidth: '100px', width: '100px', height: '150px', backgroundColor: 'cyan',
+//     }}
+//     >
+//       {name}
+//     </div>
+//   );
+// }
 
 export function handleLeftClick(ev) {
   ev.preventDefault();
@@ -34,19 +34,19 @@ export function handleRightClick(ev) {
   container.scrollBy(scrollOptions);
 }
 
-export default function Carousel() {
+export default function Carousel({ children }) {
   return (
     <div style={{ display: 'flex' }}>
-      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleLeftClick} style={{ height: '20px', marginTop: 'auto' }}>left</button>
       <div
         id="item-container"
         style={{
-          display: 'flex', gap: '15px', padding: '15px', border: '1px solid black', margin: '15px', maxWidth: '345px', overflowX: 'hidden',
+          display: 'flex', gap: '15px', padding: '15px', border: '1px solid black', margin: '15px', maxWidth: '235px', overflowX: 'hidden',
         }}
       >
-        {fakeData.map((name) => <Product name={name} />)}
+        {children}
       </div>
-      <button onClick={handleRightClick}>right</button>
+      <button onClick={handleRightClick} style={{ height: '20px', marginTop: 'auto' }}>right</button>
     </div>
   );
 }
