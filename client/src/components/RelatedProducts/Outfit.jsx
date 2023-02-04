5560b42a64889f5dd12be609826671f7e6236acc
Kathy Ye <kathyye15@gmail.com>
2023-02-03 21:32:40 -0800

    AddToOutfit sale prices implemented

--- client/src/components/RelatedProducts/Outfit.jsx
+++ client/src/components/RelatedProducts/Outfit.jsx
@@ -6,8 +6,6 @@ import getAverage from '../../helper/getAverage';
 export default function Outfit({
   style, metadata, curProduct,
 }) {
-  console.log('this is style', style);
-  console.log('this is metadata', metadata);
   const photo = style.photos[0].url;
 
   return (
@@ -16,10 +14,23 @@ export default function Outfit({
         {photo ? <img className="productPhoto" src={photo} alt="primary product style" style={{ width: '200px', height: '225px', objectFit: 'cover' }} /> : <img className="productPhoto" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png" alt="not found" style={{ width: '200px', height: '225px', objectFit: 'cover' }} />}
         <div>{curProduct.category}</div>
         <div>{style.name}</div>
-        <div>
-          $
-          {style.original_price}
-        </div>
+        {!style.sale_price ? (
+          <div>
+            $
+            {style.original_price}
+          </div>
+        ) : (
+          <>
+            <div style={{ color: 'red' }}>
+              $
+              {style.sale_price}
+            </div>
+            <div style={{ textDecoration: 'line-through' }}>
+              $
+              {style.original_price}
+            </div>
+          </>
+        )}
         <div>
           {getAverage(metadata.ratings)}
           /5 ★★★★★
