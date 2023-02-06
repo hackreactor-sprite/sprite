33c7f9c594966825fb0b765b7a2b51a65ab9d3b6
samuel ho <samuelho343@gmail.com>
2023-02-05 19:26:39 -0800

    revert before redux - new redux branch

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Provider } from 'react-redux';
-import { store } from './store';
 
 import App from './App';
 import '../../styles/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
--- client/src/store.jsx
+++
@@ -1,4 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import './layout/ratingSlice';
-
-export const store = configureStore({ reducer: {} });
