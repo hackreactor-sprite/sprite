718305ff4b1bf0b72e2b2a2069c26853ee57f1a9
Kathy Ye <kathyye15@gmail.com>
2023-02-07 20:45:41 -0800

    completed handleOutfits test

--- client/src/helper/handleOutfits.test.js
+++ client/src/helper/handleOutfits.test.js
@@ -1,9 +1,9 @@
 import {
   beforeEach, describe, expect, test,
 } from '@jest/globals';
-import { handleAddOutfit } from './handleOutfits';
+import { handleAddOutfit, handleDeleteOutfit } from './handleOutfits';
 
-describe('handleOutfits function test', () => {
+describe('handleAddOutfits helper test', () => {
   let preventDefaultMock;
   let mockEv;
   let mockOutfits;
@@ -13,11 +13,6 @@ describe('handleOutfits function test', () => {
   beforeEach(() => {
     preventDefaultMock = jest.fn();
     mockEv = {
-      target: {
-        parentElement: {
-          id: 1,
-        },
-      },
       preventDefault: preventDefaultMock,
     };
     mockOutfits = [
@@ -46,5 +41,50 @@ describe('handleOutfits function test', () => {
     curStyle = { style_id: 0 };
     handleAddOutfit(mockEv, curStyle, mockOutfits, setOutfitsMock);
     expect(setOutfitsMock).not.toHaveBeenCalled();
+  });
+});
+
+describe('handleDeleteOutfits helper test', () => {
+  let preventDefaultMock;
+  let mockEv;
+  let mockOutfits;
+  let setOutfitsMock;
+
+  beforeEach(() => {
+    preventDefaultMock = jest.fn();
+    mockEv = {
+      target: {
+        parentElement: {
+          id: 1,
+        },
+      },
+      preventDefault: preventDefaultMock,
+    };
+    mockOutfits = [
+      { style_id: 0 },
+      { style_id: 1 },
+      { style_id: 2 },
+    ];
+    setOutfitsMock = jest.fn();
+  });
+
+  test('Should call preventDefault on the first argument', () => {
+    // Run function to check if mockEv.preventDefault was run
+    handleDeleteOutfit(mockEv, mockOutfits, setOutfitsMock);
+    expect(preventDefaultMock).toHaveBeenCalled();
+  });
+
+  test('Should call setOutfits function', () => {
+    handleDeleteOutfit(mockEv, mockOutfits, setOutfitsMock);
+    expect(setOutfitsMock).toHaveBeenCalled();
+  });
+
+  test('Should call setOutfits function with filtered outfits', () => {
+    const expectedArray = [
+      { style_id: 0 },
+      { style_id: 2 },
+    ];
+    handleDeleteOutfit(mockEv, mockOutfits, setOutfitsMock);
+    expect(setOutfitsMock.mock.calls[0][0]).toMatchObject(expectedArray);
   });
 });
