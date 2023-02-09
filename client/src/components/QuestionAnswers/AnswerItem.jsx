eea818ca0512f8923f6fcfbb6f1c5b3569067d52
samuel ho <samuelho343@gmail.com>
2023-02-09 10:20:32 -0800

    tests

---
+++ client/src/components/QuestionAnswers/AnswerItem.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import Helpful from '../reusable/Helpful';
+import Report from '../reusable/Report';
+import ModalRoute from '../Modal/ModalRoute';
+import Modal from '../reusable/Modal';
+
+function AnswerItem({ answer }) {
+  const [showModal, setShowModal] = useState(false);
+  const [reported, setReported] = useState(false);
+  const [curPhoto, setCurPhoto] = useState('');
+
+  const options = {
+    month: 'long',
+    year: 'numeric',
+    day: 'numeric',
+  };
+  const convertedDate = new Date(answer.date).toLocaleDateString(
+    undefined,
+    options,
+  );
+
+  const handleImage = (photo) => {
+    setCurPhoto(photo);
+    setShowModal(!showModal);
+  };
+
+  return (
+    <div className="QA-answer-container">
+      <div className="QA-answer-header" role="heading" aria-level="4">
+        <h5 className="QA-title">A:</h5>
+      </div>
+      <div className="QA-answer-body">
+        <p aria-labelledby="answer">{answer.body}</p>
+        <div
+          className="answer-photo-container"
+          role="img"
+          aria-label="answer-photo"
+        >
+          {answer.photos.map((photo, i) => (
+            <div key={i}>
+              <img
+                src={photo}
+                alt="Answer Image"
+                className="answer-photo"
+                onClick={() => handleImage(photo)}
+              />
+            </div>
+          ))}
+          {showModal &&
+            createPortal(
+              <Modal showModal={showModal} setShowModal={setShowModal} key={i}>
+                <ModalRoute route="ImageExpand" content={{ photo: curPhoto }} />
+              </Modal>,
+              document.body,
+            )}
+        </div>
+        <div className="small-container QA-answer-detail">
+          <small>
+            {'by '}
+            {answer.answerer_name}
+            {', '}
+            {convertedDate}
+          </small>
+          <Helpful helpful={answer.helpfulness} answerid={answer.id} />
+          {!reported ? (
+            <Report
+              id={answer.id}
+              type="answers"
+              setReported={setReported}
+              reported={reported}
+            />
+          ) : (
+            <small>Reported</small>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+AnswerItem.propTypes = {
+  answer: PropTypes.shape({
+    id: PropTypes.number,
+    date: PropTypes.string,
+    answerer_name: PropTypes.string,
+    body: PropTypes.string,
+    helpfulness: PropTypes.number,
+  }).isRequired,
+};
--- client/src/components/QuestionAnswers/QAItem.test.jsx
+++ client/src/components/QuestionAnswers/QAItem.test.jsx
@@ -1,139 +1,69 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import QAItem from './QAItem.test.jsx';
+import QAItem from './QAItem';
 import QuestionAnswer from '../../layout/QuestionAnswer';
 
 const mockProduct = {
-  QA: {
-    question_id: 644993,
-    question_body: 'new question who dis?',
-    question_date: '2023-02-06T00:00:00.000Z',
-    asker_name: 'richard',
-    question_helpfulness: 2,
-    reported: false,
-    answers: {
-      5990533: {
-        id: 5990533,
-        body: 'Are these shoes gonna make my feet look bigger?',
-        date: '2023-02-06T00:00:00.000Z',
-        answerer_name: 'not a seller',
-        helpfulness: 0,
-        photos: [
-          'https://web.postman.co/workspace/My-Workspace~d81eadb3-83ef-4227-8324-c804294a8fb0/request/25221147-886f4df6-0444-488d-a981-8bb688187b97',
-        ],
-      },
-      5990534: {
-        id: 5990534,
-        body: 'Are these shoes gonna make my feet look bigger?',
-        date: '2023-02-06T00:00:00.000Z',
-        answerer_name: 'not a seller',
-        helpfulness: 0,
-        photos: [
-          'https://web.postman.co/workspace/My-Workspace~d81eadb3-83ef-4227-8324-c804294a8fb0/request/25221147-886f4df6-0444-488d-a981-8bb688187b97',
-        ],
-      },
-      5990535: {
-        id: 5990535,
-        body: 'Are these shoes gonna make my feet look bigger?',
-        date: '2023-02-06T00:00:00.000Z',
-        answerer_name: 'seller',
-        helpfulness: 0,
-        photos: [
-          'https://web.postman.co/workspace/My-Workspace~d81eadb3-83ef-4227-8324-c804294a8fb0/request/25221147-886f4df6-0444-488d-a981-8bb688187b97',
-        ],
-      },
-      5990536: {
-        id: 5990536,
-        body: 'Are these shoes gonna make my feet look bigger?',
-        date: '2023-02-06T00:00:00.000Z',
-        answerer_name: 'seller',
-        helpfulness: 0,
-        photos: [
-          'https://web.postman.co/workspace/My-Workspace~d81eadb3-83ef-4227-8324-c804294a8fb0/request/25221147-886f4df6-0444-488d-a981-8bb688187b97',
-        ],
-      },
-      5990537: {
-        id: 5990537,
-        body: 'Are these shoes gonna make my feet look bigger?',
-        date: '2023-02-06T00:00:00.000Z',
-        answerer_name: 'seller',
-        helpfulness: 0,
-        photos: [
-          'https://web.postman.co/workspace/My-Workspace~d81eadb3-83ef-4227-8324-c804294a8fb0/request/25221147-886f4df6-0444-488d-a981-8bb688187b97',
-        ],
-      },
-      5990538: {
-        id: 5990538,
-        body: 'Are these shoes gonna make my feet look bigger?',
-        date: '2023-02-06T00:00:00.000Z',
-        answerer_name: 'seller',
-        helpfulness: 0,
-        photos: [
-          'https://web.postman.co/workspace/My-Workspace~d81eadb3-83ef-4227-8324-c804294a8fb0/request/25221147-886f4df6-0444-488d-a981-8bb688187b97',
-        ],
-      },
-      5990539: {
-        id: 5990539,
-        body: 'Are these shoes gonna make my feet look bigger?',
-        date: '2023-02-06T00:00:00.000Z',
-        answerer_name: 'seller',
-        helpfulness: 0,
-        photos: [
-          'https://web.postman.co/workspace/My-Workspace~d81eadb3-83ef-4227-8324-c804294a8fb0/request/25221147-886f4df6-0444-488d-a981-8bb688187b97',
-        ],
-      },
+  id: 40346,
+  campus: 'hr-rfp',
+  name: 'Morning Joggers',
+  slogan: 'Make yourself a morning person',
+  description:
+    "Whether you're a morning person or not.  Whether you're gym bound or not.  Everyone looks good in joggers.",
+  category: 'Pants',
+  default_price: '40.00',
+  created_at: '2021-08-13T14:38:44.509Z',
+  updated_at: '2021-08-13T14:38:44.509Z',
+  features: [
+    {
+      feature: 'Fabric',
+      value: '100% Cotton',
     },
-  },
-  curProduct: {
-    id: 40346,
-    campus: 'hr-rfp',
-    name: 'Morning Joggers',
-    slogan: 'Make yourself a morning person',
-    description:
-      "Whether you're a morning person or not.  Whether you're gym bound or not.  Everyone looks good in joggers.",
-    category: 'Pants',
-    default_price: '40.00',
-    created_at: '2021-08-13T14:38:44.509Z',
-    updated_at: '2021-08-13T14:38:44.509Z',
-    features: [
-      {
-        feature: 'Fabric',
-        value: '100% Cotton',
-      },
-      {
-        feature: 'Cut',
-        value: 'Skinny',
-      },
-    ],
-  },
+    {
+      feature: 'Cut',
+      value: 'Skinny',
+    },
+  ],
 };
+
+const mockList = [
+  {
+    question_id: 573540,
+    question_body: 'Where Is the chainmail?',
+    question_date: '2022-02-22T00:00:00.000Z',
+    asker_name: 'Din',
+    question_helpfulness: 0,
+    reported: false,
+    answers: {},
+  },
+  {
+    question_id: 426098,
+    question_body: 'who let the cats out?',
+    question_date: '2021-09-18T00:00:00.000Z',
+    asker_name: 'jonathan',
+    question_helpfulness: 0,
+    reported: false,
+    answers: {},
+  },
+];
 
 describe('QA Item', () => {
   beforeEach(() => {
-    render(<QuestionAnswer curProduct={mockProduct.curProduct} />);
+    render(<QAItem curProduct={mockProduct} QAList={mockList} />);
   });
+
   test('should open the modal clicking on add answer button', async () => {
-    await screen.getByRole('tree', { name: /list/i });
-    await screen.getByRole('treeitem', {
-      name: /q: new question who dis\? helpful\? \(2\) yes add answer qa-list load more answers/i,
-    });
     const treeitem = screen.getByRole('treeitem', {
-      name: /q: new question who dis\? helpful\? \(2\) yes add answer qa-list load more answers/i,
+      name: /q: qrqoij` slideheading add answer qa\-list/i,
     });
+
     const addAnswerButton = within(treeitem).getByRole('button', {
       name: /add answer/i,
     });
     fireEvent.click(addAnswerButton);
+    const modal = await screen.getByTestId('modal');
 
-    const modal = screen.getByTestId('modal');
     expect(modal).toBeInTheDocument();
-  });
-
-  test('loads and displays the headers for question and answers', async () => {
-    render(<QAItem curProduct={mockProduct.curProduct} QA={mockProduct.QA} />);
-
-    expect(screen.getByText('Q:')).toBeTruthy();
-    expect(screen.getByText('A:')).toBeTruthy();
   });
 });
