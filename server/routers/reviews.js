e21631d9c82e719f93fb4807b26300b2444a11e4
samuel ho <samuelho343@gmail.com>
2023-02-10 13:52:08 -0800

    bug fix router - report & helpful

--- server/routers/reviews.js
+++ server/routers/reviews.js
@@ -6,8 +6,10 @@ const router = express.Router();
 // postman example: http://localhost:3000/reviews/?sort=newest&productid=40344
 router.get('/', (req, res) => {
   controllers
-    .getAll(`reviews/?page=${req.query.page || 1}&count=${req.query.count || 5}
-    &sort=${req.query.sort}&product_id=${req.query.productid}`)
+    .getAll(
+      `reviews/?page=${req.query.page || 1}&count=${req.query.count || 5}
+    &sort=${req.query.sort}&product_id=${req.query.productid}`,
+    )
     .then((data) => {
       res.send(data.data);
     })
@@ -31,23 +33,26 @@ router.post('/', (req, res) => {
     .create('reviews', req.body)
     .then(() => {
       res.status(201).end();
-    }).catch((err) => res.send(err));
+    })
+    .catch((err) => res.send(err));
 });
 
-router.put('/:review_id/helpful', (req, res) => {
+router.put('reviews/:review_id/helpful', (req, res) => {
   controllers
     .update(`reviews/${req.params.review_id}/helpful`)
     .then(() => {
       res.status(204).end();
-    }).catch((err) => res.send(err));
+    })
+    .catch((err) => res.send(err));
 });
 
 router.put('/:review_id/report', (req, res) => {
   controllers
-    .update(`reviews/${req.params.review_id}/report`)
+    .update(`qa/reviews/${req.params.review_id}/report`)
     .then(() => {
       res.status(204).end();
-    }).catch((err) => res.send(err));
+    })
+    .catch((err) => res.send(err));
 });
 
 module.exports = router;
